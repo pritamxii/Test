2b6f8d791dcd79276f36e6041d7f74025003c584
pritamxii <papritammistry@gmail.com>
2026-04-30 09:06:48 +0530

    Update extra.js

---
+++ extra.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'ai-dark': '#0F172A',
+        'ai-soft-blue': '#38BDF8',
+        'ai-muted': '#64748B',
+      },
+      backgroundImage: {
+        'ai-gradient': 'linear-gradient(to bottom right, #1E293B, #0F172A)',
+      }
+    },
+  },
+}
